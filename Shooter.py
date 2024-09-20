09804ca4dd8b7aba7992e1f775f02073fb82c792
Vivek Sundaray <tradewithbibeksundaray@gmail.com>
2024-09-20 13:48:01 +0530

    Update Shooter.py

--- Shooter.py
+++ Shooter.py
@@ -1,1 +1,27 @@
+import pygame
 
+pygame.init()
+
+screen_width = 800
+screen_hieght = int(screen_width * 0.8)
+screen = pygame.display.set_mode((screen_width, screen_hieght))
+pygame.display.set_caption('Shooter')
+
+x=200
+y=200
+#img=pygame.image.load('img/player/Idle/0.png')
+#rect = img.get_rect()
+#rect.center=(x,y)
+
+
+run=True
+while run:
+
+    #screen.blit(img,rect) 
+
+    for event in pygame.event.get():
+        #quit game
+        if event.type == pygame.QUIT:
+            run=False
+
+pygame.quit()
